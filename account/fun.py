6ddfa82aee1507b4e89d23da0bf0316506f2bc48
shybeeJD <1639938697@qq.com>
2020-12-23 15:36:42 +0800

    second commit

---
+++ account/fun.py
@@ -0,0 +1,85 @@
+from datetime import datetime
+#import models
+from hashlib import md5
+import requests
+import json
+
+import base64
+
+def encrypt_md5(s):
+    # 创建md5对象
+    new_md5 = md5()
+    print('0')
+    # 这里必须用encode()函数对字符串进行编码，不然会报 TypeError: Unicode-objects must be encoded before hashing
+    new_md5.update(s.encode(encoding='utf-8'))
+    print(new_md5.hexdigest())
+    # 加密
+    return new_md5.hexdigest()
+
+def time_diff_now(d):
+    if d==null:
+        return 10000
+    now = datetime.now()
+    d1 = datetime.strptime(str(d), '%Y-%m-%d %H:%M:%S.%f')
+    d2 = datetime.strptime(str(now), '%Y-%m-%d %H:%M:%S.%f')
+    delta=d2-d1
+    return delta.days*24*60*60+delta.seconds
+
+def data_dif(d1,d2):
+    f=datetime.strptime(d1,'%Y-%m-%d')
+    l=datetime.strptime(d2,'%Y-%m-%d')
+    days=(l-f).days
+    return days
+def time_dif(d1,d2):
+    f=datetime.strptime(d1,'%H:%M')
+    l=datetime.strptime(d2,'%H:%M:%S')
+    second=(l-f).seconds
+    return second
+
+def get_token():
+    url='https://aip.baidubce.com/oauth/2.0/token'
+
+    data={"grant_type":"client_credentials", "client_id":"GvHXiXf1VRTSheKox1a1jy7E", "client_secret":"vjfRfVragvhNRNFw206zwdF6tlEq6BqH"}
+
+    headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"}
+
+    html = requests.post(url, headers = headers,  data=data)
+    if (html.content):
+        data=json.loads(html.text)
+    return(data['access_token'])
+
+
+def base64encode(path):
+    with open(path, 'rb') as f:
+        base64_data = base64.b64encode(f.read())
+        s = base64_data.decode()
+        return s
+
+
+
+def face_det(pic1,pic2):
+    url='https://aip.baidubce.com/oauth/2.0/token'
+
+    data={"grant_type":"client_credentials", "client_id":"GvHXiXf1VRTSheKox1a1jy7E", "client_secret":"vjfRfVragvhNRNFw206zwdF6tlEq6BqH"}
+
+    headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"}
+
+    html = requests.post(url, headers = headers,  data=data)
+    if (html.content):
+        data=json.loads(html.text)
+    access_token=(data['access_token'])
+    url='https://aip.baidubce.com/rest/2.0/face/v3/match'
+    headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"}
+    params = json.dumps(
+        [{"image": base64encode(pic1), "image_type": "BASE64", "face_type": "LIVE", "quality_control": "LOW"},
+        {"image": pic2, "image_type": "BASE64", "face_type": "IDCARD", "quality_control": "LOW"}])
+    request_url = url + "?access_token=" + access_token
+    respose=requests.post(request_url,data=params,headers=headers)
+    data=json.loads(respose.text)
+    if(data['result']['score']):
+        if (data['result']['score'])>80:
+            return True
+    return False
+
+
+print(time_dif('19:00','21:00:00'))
---
+++ mysite/urls.py
@@ -0,0 +1,43 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/dev/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.conf.urls import url
+from django.urls import path
+from django.conf.urls import include
+ 
+from . import views
+ 
+urlpatterns = [
+	path('admin/', admin.site.urls),
+	#url(r'^$', views.hello),
+	url(r'^index/', views.index),
+    url(r'^$', views.index),
+    url(r'^login/', views.login),
+    url(r'^register/', views.register),
+    url(r'^forget/', views.forget),
+    url(r'^reset_pd/', views.reset_pd),
+    url(r'^logout/', views.logout),
+    url(r'^hello/', views.hello),
+    url(r'^get_vcode/', views.get_vcode),
+    url(r'^myinformation_edit/', views.myinformation_edit),
+    url(r'^create_pro/', views.create_pro),
+    url(r'^project_enter/', views.project_enter),
+    url(r'^project_inf/', views.project_inf),
+    url(r'^check_in/', views.check_in),
+    url(r'^my_project/', views.my_project),
+    url(r'^business_apply/', views.business_apply),
+    url(r'^ask_for_leave/', views.ask_for_leave),
+]
